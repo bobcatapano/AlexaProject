ea4538400b296b4019fb25e7f0e7ef51fbed5361
bobcatapano <rob.catapano@gmail.com>
2016-09-15 17:03:05 -0600

    Add files via upload

---
+++ AlexaSkill.Data/Requests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AlexaSkill.Data
+{
+    public class Requests
+    {
+        public Request Create(Request request)
+        {
+        //    using (var db = new bobcatdatabaseEntities())
+        //    {
+        //        var member = db.Members.FirstOrDefault(m => m.AlexaUser == request.UserId);
+
+        //        if (member == null)
+        //        {
+        //            request.Member = new Member() { AlexaUser = request.UserId, CreatedDate = DateTime.UtcNow, ListRequestDate = DateTime.UtcNow, RequestCount = 1 };
+
+        //            db.Requests.Add(request);
+        //        }
+        //        else
+        //        {
+        //            member.Requests.Add(request);
+        //        }
+
+        //        db.SaveChanges();
+        //    }
+
+            return request;
+        }
+    }
+}
